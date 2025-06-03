cb3b4d7db9cdd884892a78faf5a373d39def7c6d
remybms <112947392+remybms@users.noreply.github.com>
2025-06-03 17:31:43 +0200

    format login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,15 +5,21 @@ import LoginForm from '@/components/LoginForm.vue';
 </script>
 
 <template>
-  <LoginForm/>
-  <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTZ2ZTg5MmcwdmxoZGh1OXE3eG1hZnA1NWF5d3dkZ3l5N3AxYmh4cyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/XreQmk7ETCak0/giphy.gif" alt="Pouce en l'air" class="gif"/>
+  <div class="loginPage">
+    <LoginForm />
+    <img
+      src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTZ2ZTg5MmcwdmxoZGh1OXE3eG1hZnA1NWF5d3dkZ3l5N3AxYmh4cyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/XreQmk7ETCak0/giphy.gif"
+      alt="Pouce en l'air" class="gif" />
+  </div>
 </template>
 
 <style scoped>
 .gif {
   display: flex;
 }
-#app {
+
+.loginPage {
+  height: 100vh;
   display: flex;
   flex-direction: row;
 }
@@ -24,7 +30,7 @@ import LoginForm from '@/components/LoginForm.vue';
   }
 }
 
-@media (max-width: 1024px){
+@media (max-width: 1024px) {
   #app {
     display: flex;
     flex-direction: column;
